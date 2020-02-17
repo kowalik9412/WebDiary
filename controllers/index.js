6664b9487a02e6a615251f4fa0849fdf53e7fd36
Sebastian <kowalik9412@gmail.com>
2020-02-17 21:59:52 +0000

    isAuthentication complete
    isAuthentication middleware has been added and protects user's main webpage. Users wont be able to see their main page unless they are logged in

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,5 @@
 exports.getIndexPage = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    res.render('user/main', {
-      pageTitle: 'Main Page'
-    });
-  } else {
-    req.flash('error_message', 'You are not logged in');
-    res.redirect('/auth/login');
-  }
+  res.render('user/main', {
+    pageTitle: 'Home'
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const indexController = require('../controllers/index');
 
+const authentication = require('../util/isAuth');
+
 const router = express.Router();
 
-router.get('/', indexController.getIndexPage);
+router.get('/', authentication, indexController.getIndexPage);
 
 module.exports = router;
